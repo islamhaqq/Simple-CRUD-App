2cb9ae6bb122561f96fd6502ff47483ff3c1f87b
Syed Aman <sna274@nyu.edu>
2016-12-05 18:38:56 -0500

    rename parameters to conventions

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-console.log('May the Node be with you!');
-
 // use express
 const express = require('express');
 const app = express();
@@ -7,9 +5,10 @@ const app = express();
 // create server
 app.listen(3000, () => {
     console.log('listening on port 3000');
+    console.log('May the Node be with you!');
 })
 
 // send GET request to server from browser
-app.get('/', (response, request) => {
+app.get('/', (req, res) => {
     // do something
 })
