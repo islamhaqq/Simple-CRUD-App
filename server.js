2f56296549e481df2e5d2df8c2777757fe492f09
Syed Aman <sna274@nyu.edu>
2016-12-05 18:27:56 -0500

    create server on port 3000

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ console.log('May the Node be with you!');
 // use express
 const express = require('express');
 const app = express();
+
+// create server
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
