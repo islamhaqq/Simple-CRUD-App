9fbff783994ce31a832a19f8498723c88d9a434f
Syed Aman <sna274@nyu.edu>
2016-12-05 18:44:50 -0500

    send server response on '/' path

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 // create server
 app.listen(3000, () => {
     console.log('listening on port 3000');
-    console.log('May the Node be with you!');
 })
 
 // send GET request to server from browser
 app.get('/', (req, res) => {
-    // do something
+    res.send('May the Node be with you!');
 })
